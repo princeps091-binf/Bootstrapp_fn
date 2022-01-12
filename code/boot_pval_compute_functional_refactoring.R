f62802180b0ecb3a2cacb44c09eb788263a7ba7d
princeps091-binf <princeps091@gmail.com>
2022-01-12 19:49:31 +0100

    added the error handling trick to avoid process stopping

--- code/boot_pval_compute_functional_refactoring.R
+++ code/boot_pval_compute_functional_refactoring.R
@@ -56,7 +56,7 @@ rn_feature_GRange_build_fn<-function(n_vec,fn_bed_l,hg19_coord,tmp_cage_tbl,fn_f
       clusterExport(cl,c("f","tmp_n","fn_bed_l","tmp_cage_tbl","hg19_coord"),envir = fn_env)
       
       rn_fn_coord_l[[f]]<-parLapply(cl,1:100,function(x){
-        rn_pol<-bed_shuffle(tmp_cage_tbl,genome = hg19_coord,excl = fn_bed_l[[f]],within = T,max_tries=1e9)%>%sample_n(tmp_n)
+        rn_pol<-bed_shuffle(tmp_cage_tbl,genome = hg19_coord,excl = fn_bed_l[[f]],within = T,max_tries=1e6)%>%sample_n(tmp_n)
         return(rn_pol)
       })
       stopCluster(cl)
@@ -72,11 +72,12 @@ rn_feature_GRange_build_fn<-function(n_vec,fn_bed_l,hg19_coord,tmp_cage_tbl,fn_f
       })
       clusterExport(cl,c("f","tmp_n","fn_bed_l","tmp_cage_tbl","hg19_coord"),envir = fn_env)
       rn_fn_coord_l[[f]]<-parLapply(cl,1:100,function(x){
-        rn_pol<-valr::bed_shuffle(x = tmp_cage_tbl,genome = hg19_coord,incl = fn_bed_l[[f]],within=T,max_tries=1e9)%>%sample_n(tmp_n)
+        rn_pol<-try(valr::bed_shuffle(x = tmp_cage_tbl,genome = hg19_coord,incl = fn_bed_l[[f]],within=T,max_tries=1e6)%>%sample_n(tmp_n),silent=T)
         return(rn_pol)
       })
       stopCluster(cl)
       rm(cl)
+      rn_fn_coord_l[[f]]<-rn_fn_coord_l[[f]][!(unlist(lapply(rn_fn_coord_l[[f]],function(x)any(class(x) %in% "try-error"))))]
     }
   }
   
