2827335e945ecd669e3d5febb6aaf627b964ba27
princeps091-binf <princeps091@gmail.com>
2022-03-09 09:21:50 +0100

    explicit check for detection of mutually exclusive compound hubs

--- code/finding_top_compound_5kb_hub.R
+++ code/finding_top_compound_5kb_hub.R
@@ -45,3 +45,26 @@ rm(tmp_l)
 tmp_tbl %>% 
   mutate(res=fct_relevel(res,names(res_num)),line=fct_relevel(line,c("GM12878","HMEC","H1"))) %>% 
   ggplot(.,aes(line,n,fill=res))+geom_bar(stat="identity",position="fill")+scale_fill_brewer(palette="Set1")
+#-----------------------------------
+# explicit evaluation of ancestry
+spec_res_file<-"~/Documents/multires_bhicect/data/HMEC/spec_res/"
+compund_hub_file<-"./data/candidate_compound_hub/HMEC_5kb_tss_compound_hub.Rda"
+
+compound_hub_5kb_tbl<-get_obj_in_fn(compund_hub_file)
+
+chromo<-"chr1"
+base::load(paste0(spec_res_file,chromo,"_spec_res.Rda"))
+chr_bpt<-FromListSimple(chr_spec_res$part_tree)
+node_ancestor<-chr_bpt$Get(function(x){x$Get('name',traversal='ancestor')})
+node_ancestor<-lapply(node_ancestor,'[',-1)
+tmp_tbl<-compound_hub_5kb_tbl %>% 
+  filter(chr==chromo)
+set2<-tmp_tbl$parent.hub[unlist(lapply(tmp_tbl$parent.hub,function(x){
+  !(any(tmp_tbl$parent.hub %in% node_ancestor[[x]]))
+}))]  
+
+set1<-tmp_tbl %>% 
+   filter(parent.hub %in% tmp_tbl$parent.hub[which(!(tmp_tbl$parent.hub %in% unique(unlist(tmp_tbl$ch.hub))))]) %>% 
+  dplyr::select(parent.hub) %>% unlist
+all(set1 %in% set2)
+#-----------------------------------
